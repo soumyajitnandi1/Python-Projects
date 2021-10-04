fd03935bd45c2bb20e23feb3436548f6b232cf02
Infinity_Tech <71644634+infinity-sudo@users.noreply.github.com>
2021-10-04 12:30:19 +0530

    Count of divisors

---
+++ 3.Count of Divisors.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//60=2^2*3*5 
+//no of divisors=(2+1)*(1+1)*(1+1)=12
+
+int factors(int n){
+    int res=1;
+    for (int i=2; i*i<=n; i++){
+        int count=0;
+        if(n%i==0){
+            while(n%i==0){
+                n=n/i;
+                count+=1;
+            }
+            res=res*(count+1);
+        }
+    }
+    if(n!=1){
+        res*=2;
+    }
+    return res;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<factors(n);
+    return 0;
+}
