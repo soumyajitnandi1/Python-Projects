d5dc685d908bf2860023ac9a74ed851e7ca5fa5c
Dip31032001 <92220594+Dip31032001@users.noreply.github.com>
2021-10-09 21:08:16 +0530

    Add files via upload

---
+++ 5.8.py
@@ -0,0 +1,14 @@
+import pdb
+
+def find(list1,num):
+    for each in list1:
+        if(each!=num):
+            print(each)
+        else:
+            break
+
+t=[]
+for i in range(100000):
+    t.append(i)
+
+find(t,99999)
